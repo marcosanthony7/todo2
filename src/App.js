4b40df4b18d919cce90756a088a721f10a4f41e2
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-10-10 11:59:54 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo2 from './components/Todo2';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo2></Todo2>
+    </div>
+  );
+}
+
+export default App;
